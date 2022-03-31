7c76a2e3a987d265ca4e41ca581b5ae8efa7acaa
devpenom <iphenom01@gmail.com>
2022-03-31 12:58:53 +0100

    Image fixes and typo error fixes

--- src/components/global/SocialLinks.jsx
+++ src/components/global/SocialLinks.jsx
@@ -1,14 +1,6 @@
 import React from "react";
 import "./SocialLinks.css";
-import {
-  FaTelegramPlane,
-  FaTwitter,
-  FaInstagram,
-  FaReddit,
-  FaMedium,
-  FaDiscord,
-  FaGithub,
-} from "react-icons/fa";
+import { FaTelegramPlane, FaTwitter, FaInstagram, FaReddit, FaMedium, FaDiscord, FaGithub } from "react-icons/fa";
 
 function SocialLinks({ smallIcons }) {
   return (
@@ -19,7 +11,7 @@ function SocialLinks({ smallIcons }) {
       <a href="https://twitter.com/ADADEMON_?t=gPBFY7DCnGGBy2iWngw8dg&s=09">
         <FaTwitter />
       </a>
-      <a href="https://instagram.com/ada.dem0n?utm_medium=copy_link">
+      <a href="https://instagram.com/adademonpage?utm_medium=copy_link">
         <FaInstagram />
       </a>
       <a href="https://www.reddit.com/user/Ada_demon/">
